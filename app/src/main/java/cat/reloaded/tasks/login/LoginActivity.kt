cc34673f2bc1dd8ec52044cfdb2e1f7ea3cbb5a2
Mohamed Hamdy <mohamed.hamdyeg98@gmail.com>
2020-04-11 13:22:36 +0200

    :lipstick: Login UI

---
+++ app/src/main/java/cat/reloaded/tasks/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package cat.reloaded.tasks.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cat.reloaded.tasks.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
