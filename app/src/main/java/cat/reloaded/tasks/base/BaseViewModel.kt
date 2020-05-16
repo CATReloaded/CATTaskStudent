3acf75b64addf97c6a1ee85e8e5eb97a9d22444a
Mohamed Hamdy <mohamed.hamdyeg98@gmail.com>
2020-05-16 04:29:21 +0200

    implement baseviewmodel

--- app/src/main/java/cat/reloaded/tasks/base/BaseViewModel.kt
+++ app/src/main/java/cat/reloaded/tasks/base/BaseViewModel.kt
@@ -1,7 +1,18 @@
 package cat.reloaded.tasks.base
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.CoroutineContext
 
-abstract class BaseViewModel<VA : ViewAction> : ViewModel() {
+abstract class BaseViewModel<VA : ViewAction> : ViewModel(), CoroutineScope {
+
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext = Dispatchers.IO + job
     abstract fun processAction(viewAction: VA)
+    override fun onCleared() {
+        job.cancel()
+        super.onCleared()
+    }
 }
